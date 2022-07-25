014ed90b48644dae30171a60c46109db68d0ade0
kyuri09 <kyuli0909@naver.com>
2022-07-25 19:03:20 +0900

    fix bug

--- wc.c
+++ wc.c
@@ -16,7 +16,8 @@ int main()
  FILE *fp;
  char filename[256];
  char ch;
- int linecount, wordcount, charcount;
+ int linecount, wordcount;
+ long charcount:
  struct timeval start, end;
  double totaltime;
 
@@ -38,7 +39,7 @@ int main()
 	//Repeat until End Of File character is reached.	
 	   while ((ch=getc(fp)) != EOF) {
 	   	  // Increment character count if NOT new line or space
-		    if (ch != ' ' && ch != '\n') { ++charcount; }
+		   if (ch != ' ' && ch != '\n') { ++charcount; }
 		  
 		  // Increment word count if new line or space character
 		   if (ch == ' ' || ch == '\n') { ++wordcount; }
@@ -62,7 +63,7 @@ int main()
   gettimeofday(&end, NULL);
     printf("Lines : %d \n", linecount);
     printf("Words : %d \n", wordcount);
-    printf("Characters : %d \n", charcount);
+   // printf("Characters : %d \n", charcount);
     totaltime = (((end.tv_usec - start.tv_usec) / 1.0e6 + end.tv_sec - start.tv_sec) * 1000) / 1000;
     printf("total time : %f sec\n",totaltime);
 
